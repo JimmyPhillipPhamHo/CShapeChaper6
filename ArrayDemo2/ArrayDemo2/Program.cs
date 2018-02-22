d02ba1a434e6af92e5ab83a13e6da83b2d76b934
Noble115 <jimmyphamho@yahoo.com>
2018-02-22 15:07:40 -0600

    Update 1

---
+++ ArrayDemo2/ArrayDemo2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ArrayDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] score = new int[8];
+            int x;
+            string inputString;
+            for (x = 0; x < score.Length; ++x)
+            {
+                Console.WriteLine("Enter your scores on test {0} ", x+1);
+                inputString = Console.ReadLine();
+                score[x] = Convert.ToInt32(inputString);
+            }
+            Console.WriteLine("\n-------------------------------------");
+            Console.WriteLine();
+            for (x = 0; )
+            {
+                Console.WriteLine(  );
+            }
+            Console.WriteLine("\n-------------------------------------");
+            Array.Sort(score);
+            Console.WriteLine();
+            for ()
+            {
+
+            }
+            Console.WriteLine("\n-------------------------------------");
+            Array.Reverse();
+            Console.WriteLine();
+            for ()
+            {
+
+            }
+
+
+        }
+    }
+}
